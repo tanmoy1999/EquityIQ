ccf09f3e65fa2d796599b1bb815d4addbebacdb5
Tanmoy Munshi <tanmoymunshi1999@gmail.com>
2026-02-07 21:25:49 +0530

    Remove MongoDB connection from db.py
    Removed MongoDB connection code and related imports.

--- utilities/db.py
+++ utilities/db.py
@@ -1,15 +1,1 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-import pandas as pd
-import logging
-uri = "mongodb+srv://tanmoyworkspace_db_user:Ejevnrm3gxo2XVHe@equityiq.lysmhzc.mongodb.net/?appName=EquityIQ"
 
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-# tanmoyworkspace_db_user Ejevnrm3gxo2XVHe
